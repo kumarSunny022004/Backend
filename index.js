525d14aefd759754e3f2c06de80301b8fb1071f3
KUMARSUNNY <ks9107163@gmail.com>
2025-01-11 18:44:19 +0530

    made it production ready

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+// console.log(process.env) // remove this after you've confirmed it is working
 const express = require('express')
 const app = express()
 const port =   3000
@@ -13,6 +15,6 @@ app.get('/login',(req,res)=>{
     res.send("login successfull")
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
