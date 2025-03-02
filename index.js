b56d5097e0ce917866c603b5d26bd6439d0ff01d
KUMARSUNNY <ks9107163@gmail.com>
2025-03-03 01:00:51 +0530

    new chnage

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
-const port = 4000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Suniyaa')
 })
-
+app.get('/about', (request,respone)=>{
+    respone.send('About page')
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
